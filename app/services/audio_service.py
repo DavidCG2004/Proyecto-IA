468af9fd053d61430b9ae1f8859c43235700cd7b
DavidCG2004 <Alexandergarcia215@outlook.com>
2026-01-22 20:02:10 -0500

    Refine docstring in load_audio function
    Updated docstring to clarify audio format usage.

--- app/services/audio_service.py
+++ app/services/audio_service.py
@@ -4,12 +4,13 @@ from app.config import settings
 
 def load_audio(file_path: str):
     """
-    Carga un archivo de audio y lo convierte al formato requerido.
+    Carga un archivo de audio y lo convierte al formato para ser usado
     """
 
     audio, sample_rate = librosa.load(
         file_path,
         sr=settings.SAMPLE_RATE
     )
+
 
     return audio
